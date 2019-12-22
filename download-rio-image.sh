17bc04b5f331964f7a7712ac45421a2a18242b28
David Vo <auscompgeek@users.noreply.github.com>
2019-12-22 22:34:18 +1100

    Clean up pipelines

--- download-rio-image.sh
+++ download-rio-image.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-UPDATE_SUITE_TAR=data.tar.gz
 UPDATE_SUITE_DIR=$(mktemp -d)
 OUTER_CAB_DIR=$(mktemp -d)
 INNER_ZIP_DIR=$(mktemp -d)
 
 function cleanup {
-    rm -f "$UPDATE_SUITE_TAR"
     rm -rf "$UPDATE_SUITE_DIR"
     rm -rf "$OUTER_CAB_DIR"
     rm -rf "$INNER_ZIP_DIR"
@@ -23,16 +21,14 @@ UTILITIES_VERSION=$(echo "$VERSION_TUPLE" | cut -d _ -f 3)
 FEED_URL="https://download.ni.com/support/nipkg/products/ni-f/ni-frc-$YEAR-game-tools/$GAME_TOOLS_VERSION/released"
 UTILITIES_FILENAME=$(wget -q -O - "$FEED_URL/Packages" | grep "ni-frc-$YEAR-utilities_$UTILITIES_VERSION.*\.nipkg" | cut -d ' ' -f 2- | tr -d '\r')
 
-wget -q -O - "$FEED_URL/$UTILITIES_FILENAME" | bsdtar -xf - "$UPDATE_SUITE_TAR"
+wget -q -O - "$FEED_URL/$UTILITIES_FILENAME" |
+    bsdtar -xOf - data.tar.gz |
+    bsdtar -zxf - -C "$UPDATE_SUITE_DIR"
 
-bsdtar -zxf "$UPDATE_SUITE_TAR" -C "$UPDATE_SUITE_DIR"
+cabextract -q "$UPDATE_SUITE_DIR/FRC_U00/FRC_U00.cab" -d "$OUTER_CAB_DIR"
 
-OUTER_CAB=$(find "$UPDATE_SUITE_DIR" -path '*/FRC_U00/*.cab' -exec du {} + | sort -n | tail -n 1 | awk '{$1=""; print substr($0, 2)}')
-cabextract -q "$OUTER_CAB" -d "$OUTER_CAB_DIR"
+INNER_ZIP=$(du "$OUTER_CAB_DIR"/* | sort -nr | head -1 | cut -f2)
+unzip -q "$INNER_ZIP" "roboRIO_*.zip"
+unzip -q roboRIO_*.zip systemimage.tar.gz
 
-INNER_ZIP=$(find "$OUTER_CAB_DIR" -type f -exec du {} + | sort -n | tail -n 1 | awk '{$1=""; print substr($0, 2)}')
-unzip -q "$INNER_ZIP" -d "$INNER_ZIP_DIR"
-unzip -q "$INNER_ZIP_DIR/*.zip" -d "$INNER_ZIP_DIR"
-
-basename $(ls "$INNER_ZIP_DIR/"*.zip) | cut -c 9- | rev | cut -c 5- | rev > DOCKER_TAG
-cp "$INNER_ZIP_DIR/systemimage.tar.gz" .
+basename roboRIO_*.zip .zip | cut -c 9- > DOCKER_TAG
